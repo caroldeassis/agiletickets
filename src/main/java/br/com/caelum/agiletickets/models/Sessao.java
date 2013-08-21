3147dad979d22ec940272bacca9ab5f69b3a75cb
cici <cici@gmail.com>
2013-08-21 11:57:30 -0300

    refatorado metodo podeReservar

--- src/main/java/br/com/caelum/agiletickets/models/Sessao.java
+++ src/main/java/br/com/caelum/agiletickets/models/Sessao.java
@@ -104,8 +104,6 @@ public class Sessao {
 
 	public boolean podeReservar(Integer numeroDeIngressos) {
 		int sobraram = getIngressosDisponiveis() - numeroDeIngressos;
-        boolean naoTemEspaco = sobraram < 0;
-
-        return !naoTemEspaco;
+        return sobraram >= 0;
 	}
 }
